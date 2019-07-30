9d65be934a21a7a3c1f58050d639eedcefb302df
Alexander Schuster <alexander.schuster@web.de>
2019-07-30 23:38:59 +0200

    fix video and canvas size

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,11 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SaveImageService } from '../save-image/save-image.service';
 import { ReadExcelService } from '../read-excel/read-excel.service';
 
+// TODO ausgewählten dateiname speichern statt image.png
+// TODO .exe icon aktuell angular logo -> evtl bss logo nehmen?
+// TODO mit Maustasten oben/unten durch Klassen/Namen switchen
+// TODO einstellungs menü für laufwerk erstellen
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,29 +16,27 @@ export class HomeComponent implements OnInit {
   @ViewChild('webcam', {static: true}) webcamVideo: ElementRef<HTMLVideoElement>;
   @ViewChild('canvas', {static: true}) canvas: ElementRef<HTMLCanvasElement>;
 
-  private canvasCtx: CanvasRenderingContext2D;
+  private readonly SQUARESIZE = 500;
 
   constructor(private saveImageService: SaveImageService, private readExcelService: ReadExcelService) { }
 
   ngOnInit() {
-    const constraints = {
+    const webcamConfig = {
       audio: false,
       video: {
         mandatory: {
-          maxHeight: 480,
-          maxWidth: 800,
-          minHeight: 480,
-          minWidth: 800,
+          maxHeight: this.SQUARESIZE,
+          maxWidth: this.SQUARESIZE,
+          minHeight: this.SQUARESIZE,
+          minWidth: this.SQUARESIZE,
         }
       }
     };
 
     navigator.getUserMedia(
-      constraints as any,
+      webcamConfig as any,
       stream => this.webcamVideo.nativeElement.srcObject = stream,
       error => console.error(error));
-
-    this.canvasCtx = this.canvas.nativeElement.getContext('2d');
   }
 
   get klassen(): string[] {
@@ -41,18 +44,28 @@ export class HomeComponent implements OnInit {
   }
 
   capture(): void {
-    this.canvasCtx.drawImage(this.webcamVideo.nativeElement, 0, 0, 640, 480);
-    const url = this.canvas.nativeElement.toDataURL('image/jpg', 0.8);
-    const base64Data = url.replace(/^data:image\/png;base64,/, '');
-    // this.saveImageService.writeImage(base64Data);
+    this.drawImage();
+    this.saveImage();
+
     console.log('diana stinkt');
   }
 
   delete(): void {
     console.log('diana lümmelt rum');
+
+    // TODO löscht nicht das gespeicherte Bild auf dem Laufwerk, so ok?
   }
 
-  submit(): void {
-    console.log('diana macht Lärm');
+  drawImage(): void {
+    const canvasCtx = this.canvas.nativeElement.getContext('2d');
+    canvasCtx.drawImage(this.webcamVideo.nativeElement, 0, 0, this.SQUARESIZE, this.SQUARESIZE);
+  }
+
+  saveImage(): void {
+    // TODO speichert das aufgenommene automatisch, denkbar auch ein weiterer 'Foto abspeichern'.
+    //  Die Idee wäre hierbei erst ein paar Schnappschüsse auszuprobieren und sich dann explizit auf siene Auswahl festlegen kann.
+    const url = this.canvas.nativeElement.toDataURL('image/jpg', 0.8);
+    const base64Data = url.replace(/^data:image\/png;base64,/, '');
+    this.saveImageService.writeImage(base64Data);
   }
 }
--- src/app/save-image/save-image.service.ts
+++ src/app/save-image/save-image.service.ts
@@ -8,6 +8,7 @@ import * as fs from 'fs';
 export class SaveImageService {
 
   writeImage(image: string): void {
-    fs.writeFileSync(join('/Users/ftischler/Desktop', 'image.png'), image, 'base64');
+    // fs.writeFileSync(join('/Users/ftischler/Desktop', 'image.png'), image, 'base64');
+    fs.writeFileSync(join('/Users/alexanderschuster/Desktop', 'image.png'), image, 'base64');
   }
 }
