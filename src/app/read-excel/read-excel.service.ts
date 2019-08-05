64122c2600f9f590dd41ff6b67fde3db59314efc
Alexander Schuster <alexander.schuster@web.de>
2019-08-05 22:52:18 +0200

    refactor read excel service to read set up columns dynamically

--- src/app/read-excel/read-excel.service.ts
+++ src/app/read-excel/read-excel.service.ts
@@ -11,18 +11,19 @@ export class ReadExcelService {
   private readonly DATEFORMAT = 'dd"."mm"."yyyy';
 
   private data: SchuelerMitKlasse[];
+  // private data: Map<string, Schueler[]> = new Map();
 
   constructor() {
     this.parseExcel();
   }
 
-  colNumber(key: string) {
+  col(key: string): string {
     return JSON.parse(window.localStorage.getItem(key));
   }
 
-  minmaxCols(): {min: string, max: string} {
+  minmaxCols(): { min: string, max: string } {
     const sortedCols = ['vornameSpalte', 'nachnameSpalte', 'gebdatumSpalte', 'klasseSpalte'].map(key => JSON.parse(window.localStorage.getItem(key))).sort();
-    return {min: sortedCols[0], max: sortedCols[sortedCols.length - 1]};
+    return {min: sortedCols[ 0 ], max: sortedCols[ sortedCols.length - 1 ]};
   }
 
   getKlassen(): Set<string> { // TODO return undef if no data and handle in home cmp, also for other getter
@@ -43,16 +44,26 @@ export class ReadExcelService {
   parseExcel(): void {
     // TODO only execute this when (valid) excel found
     if (true) {
-      const wb: WorkBook = readFile(join(JSON.parse(window.localStorage.getItem('excelPath'))), {type: 'string', dateNF: this.DATEFORMAT});
+      const wb: WorkBook = readFile(join(JSON.parse(window.localStorage.getItem('excelPath'))), {
+        type: 'string',
+        dateNF: this.DATEFORMAT
+      });
       const ws: WorkSheet = wb.Sheets[ wb.SheetNames[ 0 ] ];
 
       // limit range to the needed columns
-      let range = utils.decode_range(ws['!ref']);
+      let range = utils.decode_range(ws[ '!ref' ]);
       range.s.c = utils.decode_col(this.minmaxCols().min);
       range.e.c = utils.decode_col(this.minmaxCols().max);
       const new_range = utils.encode_range(range);
-      // use default header still, then map as wanted (or consider "A", then loop over first row to get relevant cols, then filter out everything not needed)
-      this.data = utils.sheet_to_json(ws, {header: ['vorname', 'nachname', 'gebdatum', 'klasse'], raw: false, range: new_range}).filter((_, index) => index !== 0) as SchuelerMitKlasse[];
+      this.data = utils.sheet_to_json(ws, {header: 'A', raw: false, range: new_range})
+        .filter((_, index) => index !== 0)
+        .map(schueler => ({
+          vorname: schueler[ this.col('vornameSpalte') ],
+          nachname: schueler[ this.col('nachnameSpalte') ],
+          gebdatum: schueler[ this.col('gebdatumSpalte') ],
+          klasse: schueler[ this.col('klasseSpalte') ]
+        }));
+      console.log(this.data);
     } else {
       console.log('wurstfinger');
     }
